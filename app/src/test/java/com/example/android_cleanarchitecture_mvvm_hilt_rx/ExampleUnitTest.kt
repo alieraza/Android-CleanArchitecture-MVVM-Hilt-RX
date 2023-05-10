49b2ade6caa087e8187d5ec6fe5341d3196edeb4
Mac User <macuser@MacBook-Pro.local>
2023-05-10 17:19:31 +0500

    Initial commit

---
+++ app/src/test/java/com/example/android_cleanarchitecture_mvvm_hilt_rx/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.android_cleanarchitecture_mvvm_hilt_rx
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
